077e935da43d6f70ecadeaab51a805a330a43617
minji2219 <200146@jnu.ac.kr>
2024-05-21 20:38:47 +0900

    search 구현 및 omdbapi연결

---
+++ movie-app/src/routes/Home.js
@@ -0,0 +1,12 @@
+import Headline from "../components/Headline";
+import Search from "../components/Search";
+import { Component } from "../core/heropy";
+
+export default class Home extends Component {
+  render() {
+    const headline = new Headline().el;
+    const search = new Search().el;
+    this.el.classList.add("container");
+    this.el.append(headline, search);
+  }
+}

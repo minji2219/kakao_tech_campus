52ffeb5046a4dc57b67cdece3787781d486074ee
minji2219 <200146@jnu.ac.kr>
2024-05-24 16:30:48 +0900

    영화정보 가져와서 보여주기

--- movie-app/src/routes/Home.js
+++ movie-app/src/routes/Home.js
@@ -1,4 +1,5 @@
 import Headline from "../components/Headline";
+import MovieList from "../components/MovieList";
 import Search from "../components/Search";
 import { Component } from "../core/heropy";
 
@@ -6,7 +7,9 @@ export default class Home extends Component {
   render() {
     const headline = new Headline().el;
     const search = new Search().el;
+    const movieList = new MovieList().el;
+
     this.el.classList.add("container");
-    this.el.append(headline, search);
+    this.el.append(headline, search, movieList);
   }
 }

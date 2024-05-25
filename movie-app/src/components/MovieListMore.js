30e65dfc06aabf06cac2f520179fe4a16487ed2b
minji2219 <200146@jnu.ac.kr>
2024-05-26 01:06:08 +0900

    10개 이상 영화 목록 가져오기, 로딩 화면 추가

---
+++ movie-app/src/components/MovieListMore.js
@@ -0,0 +1,27 @@
+import { Component } from "../core/heropy";
+import movieStore, { searchMovies } from "../store/moveis";
+
+export default class MovieListMore extends Component {
+  constructor() {
+    super({
+      tagName: "button",
+    });
+    movieStore.subscribe("pageMax", () => {
+      const { page, pageMax } = movieStore.state;
+      page >= pageMax
+        ? this.el.classList.add("hide")
+        : this.el.classList.remove("hide");
+      // pageMax > page
+      //   ? this.el.classList.remove("hide")
+      //   : this.el.classList.add("hide");
+    });
+  }
+
+  render() {
+    this.el.classList.add("btn", "view-more", "hide");
+    this.el.textContent = "View more..";
+    this.el.addEventListener("click", async () => {
+      await searchMovies(movieStore.state.page + 1);
+    });
+  }
+}
--- movie-app/src/routes/Home.js
+++ movie-app/src/routes/Home.js
@@ -1,5 +1,6 @@
 import Headline from "../components/Headline";
 import MovieList from "../components/MovieList";
+import MovieListMore from "../components/MovieListMore";
 import Search from "../components/Search";
 import { Component } from "../core/heropy";
 
@@ -8,8 +9,9 @@ export default class Home extends Component {
     const headline = new Headline().el;
     const search = new Search().el;
     const movieList = new MovieList().el;
+    const movieListMore = new MovieListMore().el;
 
     this.el.classList.add("container");
-    this.el.append(headline, search, movieList);
+    this.el.append(headline, search, movieList, movieListMore);
   }
 }

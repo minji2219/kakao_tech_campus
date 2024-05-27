4f5f8f4a3599a235773234bbe6d781b4dcdf3874
minji2219 <200146@jnu.ac.kr>
2024-05-27 10:39:29 +0900

    검색한 제목에 해당하는 영화가 없을 경우, 영화 없음을 띄워줌

--- movie-app/src/components/MovieListMore.js
+++ movie-app/src/components/MovieListMore.js
@@ -18,6 +18,7 @@ export default class MovieListMore extends Component {
   }
 
   render() {
+    this.el.classList.add("hide");
     this.el.classList.add("btn", "view-more", "hide");
     this.el.textContent = "View more..";
     this.el.addEventListener("click", async () => {
